c77c3235193b32c08d89af064c55d46c4a63da0b
leandro-mandu <leandro.om15@gmail.com>
2023-10-22 12:39:41 -0300

    Projeto default

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Copa 22"
+include(":app")
+ 
